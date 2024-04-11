eb2894789884f906525dd82ed9c5e945b14be106
pdarcy <pdarcy@riseup.net>
2024-04-11 12:04:36 -0400

    make cars index route and view, hard code in basic car data

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
+cars = [
+    {'make': 'Chevrolet', 'model': 'Camaro', 'year': '1972', 'colour': 'red'},
+    {'make': 'Ford', 'model': 'Mustang', 'year': '1970', 'colour': 'black'},
+]
+
+
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
 def about(request):
     return render(request, 'about.html')
+def cars_index(request):
+    return render(request, 'cars/index.html', {
+        'cars': cars
+    })
