5652365120eef5bb9ef470f7b4a431a51c749494
pdarcy <pdarcy@riseup.net>
2024-04-17 05:12:55 -0400

    create car model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Car(models.Model):
+    make = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+    year = models.IntegerField()
+    colour = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
