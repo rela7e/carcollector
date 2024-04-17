8df199ee2fed474ed2b5acd2f8a5dec993f9e292
pdarcy <pdarcy@riseup.net>
2024-04-17 05:49:15 -0400

    finish part 3

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
 class Car(models.Model):
     make = models.CharField(max_length=100)
@@ -9,6 +9,7 @@ class Car(models.Model):
     description = models.TextField(max_length=250)
 
     def __str__(self):
-        return f'{self.make} {self.model}'
-    
+        return f'{self.make} {self.model} ({self.id})'
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'car_id': self.id})
     
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from.models import Car
 
 
@@ -17,3 +17,15 @@ def cars_index(request):
 def cars_detail(request, car_id):
   car = Car.objects.get(id=car_id)
   return render(request, 'cars/detail.html', { 'car': car })
+
+class CarCreate(CreateView):
+  model = Car
+  fields = '__all__'
+
+class CarUpdate(UpdateView):
+  model = Car
+  fields = '__all__'
+
+class CarDelete(DeleteView):
+  model = Car
+  success_url = '/cars'
